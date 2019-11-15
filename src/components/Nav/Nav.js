4ca7dc0bf64c4c3d6a3d181a837e65101692d4a2
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-15 16:49:59 -0700

    changes 11-15

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class Nav extends Component {
+    constructor(){
+        super()
+
+        
+    }
+    render(){
+        return(
+            <div>
+                <button>Home</button>
+                <button>New Post</button>
+                <button>Logout</button>
+                Nav
+            </div>
+        )
+    }
+}
+export default Nav
